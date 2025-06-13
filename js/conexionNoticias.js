87cfc2e6347f136373c4cfecfc9f10038c03fbc4
kdlopez14 <dlalo05@hotmail.com>
2025-06-13 19:54:24 +0200

    Update conexionNoticias.js

--- js/conexionNoticias.js
+++ js/conexionNoticias.js
@@ -1,42 +1,39 @@
-//Se ejecutará nada más cargue el DOM de la página
 $(document).ready(function () {
-
-    //cogemos el contenedor donde vamos a insertar las noticias 
     const contenedor = $('#cont-noticias'); 
-    var cuenta = 0; 
+    let cuenta = 0; 
 
-    //Hacemos la llamada al documento 
     $.ajax({
         url: 'https://gnews.io/api/v4/search?q=inmobiliaria&lang=es&country=es&max=10&apikey=3b43d422bb78918b23192a8dcd2924b1',
         method: 'GET',
-        success: function (data) {  //Una vez la "conexión sea correcta"<
+        success: function (data) {
+            console.log(data); // Para ver la estructura real
+            if (data.articles && data.articles.length > 0) {
                 data.articles.forEach(noticia => {
-                    if(cuenta <= 5){
-                    const div = $('<div id="tarjetas" class="border20" ></div>'); //añadimos un nuevo div dentro del contenedor 
-                    //cambiamos el formato de la fecha antes de añadirlo 
-                    var fecha = new Date(`${noticia.publishedAt}`).toLocaleDateString();
-                    
-                    //escribimos el codigo que queremos insertar en el nuevo div
-                    const nuevoCod = ` 
-                        <div class="tamImg">
-                        <img src="${noticia.image}" alt="Foto noticia" class="border20" onerror="this.onerror=null; this.src='../imagenes/noticias.png'; "> 
-                        </div> 
-                        <div>    
-                        <h3>${noticia.title}</h3>
-                        <p>${fecha}</p>
-                        <hr>
-                        <p>${noticia.description}</p>
-                        </div>
-                        <p><a href="${noticia.url}" target="_blank">Saber más +</a></p>
-                    `;
-                    div.html(nuevoCod);
+                    if(cuenta < 5){
+                        const div = $('<div id="tarjetas" class="border20"></div>');
+                        var fecha = new Date(noticia.publishedAt).toLocaleDateString();
+                        const imgUrl = noticia.image ? noticia.image.replace('http://', 'https://') : '../imagenes/noticias.png';
 
-                    //Añadimos el nuevo div a su contenedor 
-                    contenedor.append(div);
-                        cuenta ++; 
-                }
+                        const nuevoCod = ` 
+                            <div class="tamImg">
+                                <img src="${imgUrl}" alt="Foto noticia" class="border20" onerror="this.onerror=null; this.src='../imagenes/noticias.png';"> 
+                            </div> 
+                            <div>    
+                                <h3>${noticia.title}</h3>
+                                <p>${fecha}</p>
+                                <hr>
+                                <p>${noticia.description}</p>
+                            </div>
+                            <p><a href="${noticia.url}" target="_blank">Saber más +</a></p>
+                        `;
+                        div.html(nuevoCod);
+                        contenedor.append(div);
+                        cuenta++;
+                    }
                 });
-           
+            } else {
+                contenedor.append('<p>No hay noticias disponibles.</p>');
+            }
         },
         error: function (xhr, status, error) {
             console.error("Error en la petición AJAX:", error);
@@ -44,4 +41,3 @@ $(document).ready(function () {
         }
     });
 });
-
